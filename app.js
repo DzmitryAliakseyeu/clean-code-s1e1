7a7f71ed481003e1769366380eb5d822709ebb2a
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-02-07 11:15:01 +0100

    refactor: add single style and meaningful names for selectors in .js, .html, .css files according rules 3.1 and 3.2 in html-and-css.md

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
+var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
 //New task list item
@@ -65,7 +65,7 @@ var editTask=function(){
   var editInput=listItem.querySelector('input[type=text]');
   var label=listItem.querySelector("label");
   var editBtn=listItem.querySelector(".edit");
-  var containsClass=listItem.classList.contains("editMode");
+  var containsClass=listItem.classList.contains("edit-text");
   //If class of the parent is .editmode
   if(containsClass){
     //switch to .editmode
@@ -77,7 +77,7 @@ var editTask=function(){
     editBtn.innerText="Save";
   }
   //toggle .editmode on the parent.
-  listItem.classList.toggle("editMode");
+  listItem.classList.toggle("edit-text");
 };
 
 //Delete task.
